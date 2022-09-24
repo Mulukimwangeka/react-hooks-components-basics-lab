3cf110f29d378beb0c63cba9c031a8c782914ff9
mulukimwangeka <mwangekaruth@gmail.com>
2022-09-24 20:34:49 +0300

    Test are passing

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
+      <a href="#about">About</a>
     </nav>
   );
 }
@@ -17,14 +18,22 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home/>
       {/* add your <About> component here */}
+      <About/>
     </div>
   );
 }
